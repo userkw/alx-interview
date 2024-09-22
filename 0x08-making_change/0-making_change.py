9969f9d3189858e2d45b2f96a4897f5b7ca3b650
userkw2 <kawthar.elk123@gmail.com>
2024-09-22 06:54:30 -0700

    done

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
 
+""" makeChange function """
 
 def makeChange(coins, total):
-    
+    """
+    Calculates minimum coins needed for a total.
+    Returns: coin count or -1 if impossible.
+    """
     if not coins or coins is None:
         return -1
     if total <= 0:
         return 0
-    rem = 0
+    var = 0
     coins = sorted(coins)[::-1]
     for coin in coins:
         while coin <= total:
             total -= coin
-            rem += 1
+            var += 1
         if (total == 0):
-            return rem
+            return var
     return -1
